40cfd2be19cf84735e483cacda3d35e9ca1e4ab3
Zhi Wei <zhiweilee1991@gmail.com>
2019-06-05 11:46:54 +0700

    Refactored

--- helper.py
+++ helper.py
@@ -11,10 +11,13 @@ WD_PATH = Path(".")
 DATA_PATH = WD_PATH / "Data"
 if not DATA_PATH.exists():
     DATA_PATH.mkdir()
-devkit_url = "https://ai.stanford.edu/~jkrause/cars/car_devkit.tgz"
+CAR_TRAIN_URL = "http://imagenet.stanford.edu/internal/car196/cars_train.tgz"
+CAR_TEST_URL = "http://imagenet.stanford.edu/internal/car196/cars_test.tgz"
+DEVKIT_URL = "https://ai.stanford.edu/~jkrause/cars/car_devkit.tgz"
+TEST_ANNOS_WITHLABELS_URL = "http://imagenet.stanford.edu/internal/car196/cars_test_annos_withlabels.mat"
 
 
-def dl(url, dest_dir):
+def download(url:str, dest_dir:Path) -> Path:
     assert isinstance(dest_dir, Path), "dest_dir must be a Path object"
     filename = url.split('/')[-1]
     file_path = dest_dir / filename
@@ -30,7 +33,7 @@ def dl(url, dest_dir):
         return file_path
     return file_path
 
-def untar_tgz(tgz_path, dest_dir):
+def untar_tgz(tgz_path: Path, dest_dir:Path) -> Path:
     assert isinstance(tgz_path, Path), "tgz_path must be a Path object"
     assert tgz_path.exists(), "tgz_path does not exists"
     assert tgz_path.suffix == ".tgz", "tgz_path is not a .tgz file"
@@ -39,36 +42,43 @@ def untar_tgz(tgz_path, dest_dir):
     else:
         final_path = dest_dir / tgz_path.stem
     if not final_path.exists():
-        tar = tarfile.open(tgz_path, 'r:gz')
-        tar.extractall(dest_dir)
+        with tarfile.open(tgz_path, 'r:gz') as tar:
+            tar.extractall(dest_dir)
     return final_path
 
-def dl_and_untar(url, dest_dir):
-    tar_path = dl(url, dest_dir)
+def download_and_untar(url:str, dest_dir:Path) -> Path:
+    tar_path = download(url, dest_dir)
     final_path = untar_tgz(tar_path, dest_dir)
     return final_path
 
-def get_idx2name_map():
-    devkit_path = dl_and_untar(devkit_url, DATA_PATH)
+def get_car_paths() -> (Path, Path):
+    train_path = download_and_untar(CAR_TRAIN_URL, DATA_PATH)
+    test_path = download_and_untar(CAR_TEST_URL, DATA_PATH)
+    devkit_path = download_and_untar(DEVKIT_URL, DATA_PATH)
+    download(TEST_ANNOS_WITHLABELS_URL, devkit_path)
+    return (train_path, test_path)
+
+def get_idx2name_dic() -> dict:
+    devkit_path = download_and_untar(DEVKIT_URL, DATA_PATH)
     cars_metas = scipy.io.loadmat(devkit_path / 'cars_meta.mat')
     cars_metas = cars_metas['class_names'].squeeze()
     df = pd.DataFrame(cars_metas, columns=['class_names'])
     df = df.applymap(np.squeeze)
     df.insert(loc=0, column='class_idx', value=np.arange(1, len(df)+1))
-    idx2name_map = {df.iloc[i, 0]:df.iloc[i, 1] for i in range(len(df))}
-    return idx2name_map
+    idx2name_dic = {df.iloc[i, 0]:df.iloc[i, 1] for i in range(len(df))}
+    return idx2name_dic
 
-def get_cars_df(annos_matfile):
+def get_cars_df(annos_matfile:str) -> pd.DataFrame:
     assert annos_matfile in ['cars_train_annos.mat', 'cars_test_annos_withlabels.mat'], \
            "Please select 'cars_train_annos.mat' or 'cars_test_annos_withlabels.mat'"
            
-    devkit_path = dl_and_untar(devkit_url, DATA_PATH)
+    devkit_path = download_and_untar(DEVKIT_URL, DATA_PATH)
     cars_annos = scipy.io.loadmat(devkit_path / annos_matfile)
     cars_annos = cars_annos['annotations'].squeeze()
     df = pd.DataFrame(cars_annos)
     df = df.applymap(np.squeeze)
-    idx2name_map = get_idx2name_map()
-    df['class_name'] = df['class'].map(idx2name_map)
+    idx2name_dic = get_idx2name_dic()
+    df['class_name'] = df['class'].map(idx2name_dic)
     
     return df[['fname', 'class_name', 'class']]
 
